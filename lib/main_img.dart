5e90f7c9f9a3f247d90fa65ce48a5cb5de59efc3
admin <vinod.raghuwanshi@simplelogic.in>
2019-12-06 15:27:56 +0530

    second

--- lib/main_img.dart
+++ lib/main_img.dart
@@ -1,149 +1,12 @@
-//import 'package:flutter/material.dart';
-//import 'home.dart';
-//
-//void main() => runApp(MyApp());
-//
-//class MyApp extends StatefulWidget {
-//  @override
-//  _MyAppState createState() => _MyAppState();
-//}
-//
-//class _MyAppState extends State<MyApp> {
-//  int _nabBarCurrentIndex = 0;
-//  List<Widget> _screens = [
-//    Home(),
-//    Container(
-//      child: Center(
-//        child: Text("Search"),
-//      ),
-//    ),
-//    Container(
-//      child: Center(
-//        child: Text("Add"),
-//      ),
-//    ),
-//    Container(
-//      child: Center(
-//        child: Text("Hearts"),
-//      ),
-//    ),
-//    Container(
-//      child: Center(
-//        child: Text("Profile"),
-//      ),
-//    ),
-//  ];
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return MaterialApp(
-//      debugShowCheckedModeBanner: false,
-//      home: Scaffold(
-//        appBar: AppBar(
-//          leading: Icon(
-//            Icons.camera_alt,
-//            color: Colors.black,
-//          ),
-//          backgroundColor: Colors.white,
-//          title: Center(
-//            child: Text(
-//              'Hello',
-//              style: TextStyle(color: Colors.black),
-//            ),
-//          ),
-//          actions: <Widget>[
-//            IconButton(
-//              icon: Icon(
-//                Icons.cast,
-//                color: Colors.black,
-//              ),
-//              onPressed: () {},
-//            ),
-//            IconButton(
-//              icon: Icon(
-//                Icons.library_add,
-//                color: Colors.black,
-//              ),
-//              onPressed: () {},
-//            ),
-//          ],
-//        ),
-//        body: _screens[_nabBarCurrentIndex],
-//        bottomNavigationBar: BottomNavigationBar(
-//          onTap: (int index) {
-//            setState(() {
-//              _nabBarCurrentIndex = index;
-//            });
-//          },
-//          currentIndex: _nabBarCurrentIndex,
-//          items: [
-//            BottomNavigationBarItem(
-//              icon: Icon(
-//                Icons.home,
-//                color: Colors.black,
-//              ),
-//              title: Text(
-//                'Home',
-//                style: TextStyle(color: Colors.black),
-//              ),
-//            ),
-//            BottomNavigationBarItem(
-//                icon: Icon(
-//                  Icons.search,
-//                  color: Colors.black,
-//                ),
-//              title: Text(
-//                'Home',
-//                style: TextStyle(color: Colors.black),
-//              ),
-//            ),
-//            BottomNavigationBarItem(
-//                icon: Icon(
-//                  Icons.add,
-//                  color: Colors.black,
-//                ),
-//              title: Text(
-//                'Home',
-//                style: TextStyle(color: Colors.black),
-//              ),
-//            ),
-//            BottomNavigationBarItem(
-//                icon: Icon(
-//                  Icons.headset,
-//                  color: Colors.black,
-//                ),
-//              title: Text(
-//                'Home',
-//                style: TextStyle(color: Colors.black),
-//              ),
-//            ),
-//            BottomNavigationBarItem(
-//                icon: Icon(
-//                  Icons.verified_user,
-//                  color: Colors.black,
-//                ),
-//              title: Text(
-//                'Home',
-//                style: TextStyle(color: Colors.black),
-//              ),
-//            ),
-//          ],
-//        ),
-//      ),
-//    );
-//  }
-//}
-
-
 import 'package:flutter/material.dart';
 import 'customIcons.dart';
 import 'data.dart';
 import 'dart:math';
 
 void main() => runApp(MaterialApp(
-  home: MyApp(),
-  debugShowCheckedModeBanner: false,
-));
+      home: MyApp(),
+      debugShowCheckedModeBanner: false,
+    ));
 
 class MyApp extends StatefulWidget {
   @override
@@ -169,9 +32,9 @@ class _MyAppState extends State<MyApp> {
       decoration: BoxDecoration(
           gradient: LinearGradient(
               colors: [
-                Color(0xFF1b1e44),
-                Color(0xFF2d3447),
-              ],
+            Color(0xFF1b1e44),
+            Color(0xFF2d3447),
+          ],
               begin: Alignment.bottomCenter,
               end: Alignment.topCenter,
               tileMode: TileMode.clamp)),
